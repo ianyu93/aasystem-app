c58ab0564cd2405f4a4b2d22c749f935337e7b56
Ian Yu <ian.yu@arc.com.co>
2020-12-31 17:23:42 -0500

    take out setup

--- app.py
+++ app.py
@@ -12,14 +12,6 @@ import quandl
 import requests
 from datetime import datetime, timedelta
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
-
-
 fred = Fred(api_key = "3e7ce9d3322d45b49f624720abd0f36a")
 quandl.ApiConfig.api_key = "_gTGp-_JJ9kKR7-hCGT5"
  
