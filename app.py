9a794df056ed4b7ecaf40105c3216fa7fe0c7e5c
Ian Yu <ian.yu@arc.com.co>
2020-12-31 16:30:30 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import streamlit as st
+import numpy as np
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import time 
+import yfinance as yf
+from fredapi import Fred
+import quandl
+import requests
+from datetime import datetime, timedelta
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+
+fred = Fred(api_key = "3e7ce9d3322d45b49f624720abd0f36a")
+quandl.ApiConfig.api_key = "_gTGp-_JJ9kKR7-hCGT5"
+
+st.title('My first app')
+
+true = yf.Ticker("^GSPC").history(start = '2020-12-01', period="1m")['Close']
+time.sleep(1)
+
+fig = go.Figure(go.Scatter(x=true.index, y=true,
+                          mode='lines',
+                          name='SPX 500',
+                          line_color='Red'))
+
+fig.update_layout(height=500, width=1000,
+                  title = f"Quarterly Forecast, Forecasted On {datetime.today().strftime('%Y-%m-%d')}",
+                  title_font_size = 30, title_x=0.5)
+
+st.plotly_chart(fig)
+
+
