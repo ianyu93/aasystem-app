ab055e1558627a93ce5959b013451d809a7724e1
Ian Yu <ian.yu@arc.com.co>
2020-12-31 20:09:24 -0500

    fixed

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo “\
-[general]\n\
-email = \”<ian.yu@arc.com.co>\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-process.env.PORT
-“ > ~/.streamlit/config.toml
+echo "[general]
+email = \"email@com\"
+" > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
