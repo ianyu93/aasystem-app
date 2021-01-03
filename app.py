2ecffcd78e302731ba6bc540eab6b84a023cc0fa
Ian Yu <ian.yu@arc.com.co>
2021-01-03 03:02:43 -0500

    changed the app structure to help load data faster

--- app.py
+++ app.py
@@ -8,63 +8,21 @@ import plotly.graph_objects as go
 import plotly.express as px
 import time 
 import yfinance as yf
-from fredapi import Fred
-import quandl
-import requests
 from datetime import datetime, timedelta
-import glob
-from IPython.display import display
 
 ################ Website Setup ################
 # Streamlit site configuration
 st.set_page_config(
     page_title="Asset Allocator",
     layout="wide",
-    initial_sidebar_state="auto"
-)
+    initial_sidebar_state="auto")
+################ Getting All Data ################
 
-# Real time data API keys
-fred = Fred(api_key = "3e7ce9d3322d45b49f624720abd0f36a")
-quandl.ApiConfig.api_key = "_gTGp-_JJ9kKR7-hCGT5"
+table = pd.read_csv("prediction/table.csv", index_col=0)
+table2 = pd.read_csv("prediction/table2.csv", index_col=0)
+pred = pd.read_csv("prediction/pred_port.csv", index_col=0)
+true = pd.read_csv("prediction/true_port.csv", index_col=0)
 
-################ Getting Predicted Data ################
-
-# Getting all CSV files from ./prediction
-all_files = glob.glob('prediction' + "/*.csv")
-
-# Put all dataframe into a list
-all_data = []
-for filename in all_files:
-    df = pd.read_csv(filename, index_col=0, header=0)
-    all_data.append(df)
-
-# Concatenate all dataframes in all_data list, 
-table = pd.concat(all_data, axis=1, ignore_index=False)
-
-################ Getting Real-Time Data ################
-## Get data
-start = table.index[0]
-test = '2020-12-01'
-stock = yf.Ticker("^GSPC").history(start=test)
-yields = quandl.get("USTREASURY/YIELD",start_date=test)
-usd = yf.Ticker("DX-Y.NYB").history(start=test)
-gold = quandl.get("LBMA/GOLD", start_date=test)
-wti = pd.DataFrame(fred.get_series_latest_release(
-    'DCOILWTICO'), columns=["price"])
-time.sleep(1)
-## Data Wrangling
-stock = pd.DataFrame(stock['Close'])
-stock.columns = ["SPX"]
-yields = pd.DataFrame(yields['10 YR'])
-yields.rename(columns={'10 YR': "10YR yields"}, inplace = True)
-usd = pd.DataFrame(usd['Close'])
-usd.columns = ["DXY"]
-gold = pd.DataFrame(gold['USD (AM)'])
-gold.rename(columns={'USD (AM)': "GOLD"}, inplace = True)
-wti = pd.DataFrame(wti[wti.index>=test])
-wti.columns = ["WTI"]
-## Concat
-table2 = pd.concat([stock,yields,usd,gold,wti], axis=1, ignore_index=False)
 
 ################ App Content ################
 
@@ -110,62 +68,7 @@ def plotting(pred, true):
 # Codes for Efficient Frontier were modified from: 
 # https://github.com/PyDataBlog/Python-for-Data-Science
 
-def EfficientFrontier(table):
-### Populate portfolios
-# Calculate daily and annual returns of the stocks
-    returns_daily = table.pct_change()
-    returns_quarterly = returns_daily.mean() * 21
-
-    # Get daily and covariance of returns of the stock
-    cov_daily = returns_daily.cov()
-    cov_quarterly = cov_daily * 21
-
-    # Define selected assets
-    selected = table.columns
-
-    # Empty lists to store returns, volatility and weights of imiginary portfolios
-    port_returns = []
-    port_volatility = []
-    sharpe_ratio = []
-    stock_weights = []
-
-    # Set the number of combinations for imaginary portfolios
-    num_assets = len(selected)
-    num_portfolios = 50000
-
-    # Set random seed for reproduction's sake
-    np.random.seed(101)
-
-    # Populate the empty lists with each portfolios returns,risk and weights
-    for single_portfolio in range(num_portfolios):
-        weights = np.random.random(num_assets)
-        weights /= np.sum(weights)
-        returns = np.dot(weights, returns_quarterly)
-        volatility = np.sqrt(np.dot(weights.T, np.dot(cov_quarterly, weights)))
-        sharpe = returns / volatility
-        sharpe_ratio.append(sharpe)
-        port_returns.append(returns)
-        port_volatility.append(volatility)
-        stock_weights.append(weights)
-
-    # A dictionary for Returns and Risk values of each portfolio
-    portfolio = {'Returns': port_returns,
-                'Volatility': port_volatility,
-                'Sharpe Ratio': sharpe_ratio}
-
-    # Extend original dictionary to accomodate each ticker and weight in the portfolio
-    for counter,symbol in enumerate(selected):
-        portfolio[symbol+' Weight'] = [Weight[counter] for Weight in stock_weights]
-
-    # Make a nice dataframe of the extended dictionary
-    df = pd.DataFrame(portfolio)
-
-    # Get better labels for desired arrangement of columns
-    column_order = ['Returns', 'Volatility', 'Sharpe Ratio'] + [stock+' Weight' for stock in selected]
-
-    # Reorder dataframe columns
-    df = df[column_order]
-
+def EfficientFrontier(df):
     ### Setup for plotting efficient frontier
     # find min Volatility & max sharpe values in the dataframe (df)
     min_volatility = df['Volatility'].min()
@@ -222,17 +125,14 @@ def EfficientFrontier(table):
     pred_port.columns = ['Minimum Volatility', 'Maximum Sharpe']
     return fig, pred_port
 
-
-
-
 ################ App Content ################
 page_list = ["Forecast", "Optimal Portfolio"]
 page = st.sidebar.selectbox(
     "View",
     (page_list)
 )
-pred_fig, pred_port = EfficientFrontier(table)
-true_fig, true_port = EfficientFrontier(table2)
+pred_fig, pred_port = EfficientFrontier(pred)
+true_fig, true_port = EfficientFrontier(true)
 if page == page_list[0]:
     st.title("Forecast Performance")
     st.write(plotting(table,table2))
